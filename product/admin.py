374e062466692aed698f65189002a03e1e340e50
kimlongngin <kimlong.planb@gmail.com>
2019-08-12 16:26:44 +0700

    modified:   MainView/templates/product/detail.html 	modified:   db.sqlite3 	modified:   product/__pycache__/admin.cpython-37.pyc 	modified:   product/admin.py

--- product/admin.py
+++ product/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import Product, ProductHistory, ProductUnit, ProductCategory, ProductInStock, SubProductImage, Promotion, ProductInStockHistory
 from django.contrib.auth.models import Group 
-
+from django.contrib.admin.models import LogEntry, ADDITION
+import os
 
 class ProductCategoryAdmin(admin.ModelAdmin):
 	list_display = ('name', 'created_at', 'is_status')
@@ -53,15 +54,58 @@ admin.site.register(ProductHistory, admin.ModelAdmin)
 	
 
 
+
+def delete_selected(self, request, queryset):
+	if request.method == "POST":
+		for obj in queryset:
+			obj.delete()
+			History = ProductInStockHistory.objects.create(
+				product = obj.product,
+				title = obj.title,
+				unit_title = obj.unit_title,
+				unit = obj.unit,
+				amount_per_unit =  obj.amount_per_unit,
+				description = obj.description,
+				action = 'DEL'
+			)
+			History.save()
+
+	# queryset.update(status='p')
+delete_selected.short_description = "Delete"
+# Delete selected product in stock historys
+
 class ProductInStockAdmin(admin.ModelAdmin):
+	
+
 	list_display = ('product', 'title', 'unit_title', 'unit', 'amount_per_unit', 'created_at', 'is_status')
 	search_fields = ('product__name', 'title')
+	# list_display_links = None
+	view_on_site = False
+	actions = [delete_selected]
+
+	def delete_model(self, request, obj):
+		if request.POST:
+			obj.user = request.user
+			super().delete_model(request, obj)
+			History = ProductInStockHistory.objects.create(
+			product = obj.product,
+			title = obj.title,
+			unit_title = obj.unit_title,
+			unit = obj.unit,
+			amount_per_unit =  obj.amount_per_unit,
+			description = obj.description,
+			action = 'DEL'
+		)
+		History.save()
+
 
 	def save_model(self, request, obj, form, change):
 		obj.user = request.user
 		super().save_model(request, obj, form, change)
 
-		# Copy product to product's history table "ProductInStockHistory"
+		# Copy product to product's history table "ProductInStockHistory" => Action "SAVE"
+		# 1 if check this product has in the table then, this script is for update buttion, 
+		# 2 otherwise is for save button
 		History = ProductInStockHistory.objects.create(
 			product = obj.product,
 			title = obj.title,
@@ -77,14 +121,10 @@ admin.site.register(ProductInStock, ProductInStockAdmin)
 
 
 
-
-
-
 class SubProductImageAdmin(admin.ModelAdmin):
 	list_display = ('name', 'created_at', 'is_status')
 	search_fields = ('product__name', 'name')
 admin.site.register(SubProductImage, SubProductImageAdmin)
-
 
 
 class PromotionAdmin(admin.ModelAdmin):
@@ -96,9 +136,15 @@ admin.site.register(Promotion, PromotionAdmin)
 
 
 class ProductInStockHistoryAdmin(admin.ModelAdmin):
+
 	list_display = ('product', 'title', 'unit_title', 'unit', 'amount_per_unit', 'created_at', 'is_status')
 	search_fields = ('product__name', 'title')
 
+	def delete_model(self, request, obj):
+		if request.POST:
+			obj.user = request.user
+			super().delete_model(request, obj)
+		
 admin.site.register(ProductInStockHistory, ProductInStockHistoryAdmin)
 
 # admin.site.unregister(Group)
